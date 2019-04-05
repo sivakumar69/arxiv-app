7cec549371c134d64dfd99baa4db3a131aa650c6
SivakumarVayyeti <sivakumar.vayyeti@gmail.com>
2019-04-05 01:49:46 -0400

    code is documented

--- src/components/AuthorsList.js
+++ src/components/AuthorsList.js
@@ -49,37 +49,46 @@ class AuthorsList extends Component {
   componentDidMount() {
     const url = `https://cors-anywhere.herokuapp.com/http://export.arxiv.org/api/query?id_list=${this.props.articleID}`;
 
+    // api fetch call to get data of the selected article title
     fetch(url , {method: 'GET',
       headers: {Accept: 'text/xml','Content-Type': 'application/json',},
       }).then(response => {
         response.text()
         .then(( str ) => {
           let jsonObj = parser.parse(str);
+          // api returns object for single author articles and list of objects for multiple author articles
+          // logic to get list of author objects for both single and multiple authors
           if(Array.isArray(jsonObj.feed.entry.author)){
             this.getAuthorArticleCount(jsonObj.feed.entry.author);
           }
           else{
             this.getAuthorArticleCount([jsonObj.feed.entry.author]);
           }
+          // function call to set article data
           this.setArticleData(jsonObj.feed.entry);
         });
     });
 
   }
 
+  // function for setting count of articles published by each author in past 30 days
   getAuthorArticleCount(authors) {
     for(let i=0;i<authors.length;i++){
       this.getarticleCount(authors[i].name, 0, 200);
     }
   }
 
+  // counts the articles published by the author by making recursive fetch calls
   getarticleCount(name, start, end){
     let url = `https://cors-anywhere.herokuapp.com/http://export.arxiv.org/api/query?search_query=au:${name}&sortBy=submittedDate&start=${start}&max_results=${end}`;
 
+    // conditon to check if router is matching with the component
+    // stopping conditon for the recurssive function
     if(!window.location.pathname.includes("article-details")) {
       return;
     }
 
+    // fetch call to get data of specified url
     fetch(url , {method: 'GET',
       headers: {Accept: 'text/xml','Content-Type': 'application/json',},
       }).then(response => {
@@ -87,14 +96,20 @@ class AuthorsList extends Component {
         .then(( str ) => {
           let jsonObj = parser.parse(str);
           let data = jsonObj.feed.entry;
+          // conditon to check if a new fetch call is required to fetch more data of past 30 days
+          // checking the date of the last item of the page
+          // if page's last item date and today's date difference is less than or equal to 30,
+          // fetch call will be made to get more data
           if(typeof data !== "undefined"){
             let pageLastData = data[data.length-1];
             if(this.getDaysDifference(pageLastData.published) <= 30){
               let that = this;
               setTimeout(function(){
-                that.getarticleCount(name, end+1, end+500);
-              }, 4000);
+                that.getarticleCount(name, (start+end+1), 600);
+              }, 3000);
             }
+            // final articles count is reached
+            // updates the state information
             else{
               let count = start + this.getValidDataCount(data);
               let k = this.state.authorArticleCount;
@@ -102,18 +117,20 @@ class AuthorsList extends Component {
               this.setState({authorArticleCount: k});
             }
           }
+          // else will be executed if the API does not return any data due to continuous requests with in a short period of time
+          // else will make the request to the current page util the data is being fetched
           else {
             let that = this;
-            end = end - 500;
             setTimeout(function(){
-              that.getarticleCount(name, end+1, end+300);
-            }, 4000);
+              that.getarticleCount(name, start, 600);
+            }, 7000);
           }
         });
     });
 
   }
 
+  // filters out the data which is published more than 30 days
   getValidDataCount(data){
     let count = 0;
     for(let i=0; i<data.length;i++){
@@ -127,14 +144,17 @@ class AuthorsList extends Component {
     return count;
   }
 
+  // sets sate data
   setArticleData(data) {
     this.setState({articleData: data});
   }
 
+  // handles the user click event to change the route that displays the article titles of the selected author
   handleClick(e, authorName){
-    window.location = "/author-details/" + authorName;
+    this.props.history.push("/author-details/" + authorName);
   }
 
+  // returns the days difference between two given date and the current date
   getDaysDifference(date){
     let newDate = new Date(date.split('T')[0]);
     let currentDate = new Date();
@@ -143,6 +163,7 @@ class AuthorsList extends Component {
     return days;
   }
 
+  // gets list of authors for both multiple and single author articles
   getAuthorsList(){
     let authors = [];
     if(typeof this.state.articleData !== "undefined" && Array.isArray(this.state.articleData.author)){
@@ -154,6 +175,7 @@ class AuthorsList extends Component {
     return authors;
   }
 
+  // function to sort the authors based on the article count
   getSortedAuthorsList(){
     let sortedAuthors = [];
     if(typeof this.state.authorArticleCount !== "undefined" &&
